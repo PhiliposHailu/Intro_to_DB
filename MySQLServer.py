72b0ec8eb3f550d58efd0e683d4b130fecddeb07
Philipos Hailu <hailuphilipos@gmail.com>
2024-10-20 14:46:01 -0300

    update

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,5 +1,25 @@
 import mysql.connector
 
-CREATE TABLE IF NOT EXISTS alx_book_store;
-USE alx_book_store;
+def create_database():
+    connection = None
+    cursor = None
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="*******",
+            database = "alx_book_store"
+        )
+        cursor = connection.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
+    except mysql.connector.Error as error:
+        print(f"Error: {error}")
+    finally:
+        if cursor:  
+            cursor.close()
+        if connection: 
+            connection.close()
 
+if __name__ == "__main__":
+    create_database()
