403846e3b7547afe2ebb213e94276464277e3382
Philipos Hailu <hailuphilipos@gmail.com>
2024-10-20 15:49:38 -0300

    updated a text error

--- task_6.sql
+++ task_6.sql
@@ -1,5 +1,5 @@
 USE alx_book_store;
 INSERT INTO customer()
 VALUES(2,'Blessing Malik', 'eobed@sandtech.com', '124 Happiness  Ave.'),
-(3,'Obed Ehoneah', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
-(4,'Nehemial Kamolu', 'nkamolu@sandtech.com', '124 Happiness  Ave.')
+(3,'Obed Ehoneah', 'bmalik@sandtech.com', '125 Happiness  Ave.'),
+(4,'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.')
