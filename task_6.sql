09ba9f3d890a47cf3cba2e35ba1efc4d4dc806ea
Philipos Hailu <hailuphilipos@gmail.com>
2024-10-20 15:46:58 -0300

    adding valuses to a table

---
+++ task_6.sql
@@ -0,0 +1,5 @@
+USE alx_book_store;
+INSERT INTO customer()
+VALUES(2,'Blessing Malik', 'eobed@sandtech.com', '124 Happiness  Ave.'),
+(3,'Obed Ehoneah', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+(4,'Nehemial Kamolu', 'nkamolu@sandtech.com', '124 Happiness  Ave.')
